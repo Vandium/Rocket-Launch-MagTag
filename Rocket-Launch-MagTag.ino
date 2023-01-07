0cf6b5ab7443c179ab8a400725ebb0a214de994e
Brad Dranko <bddranko@gmail.com>
2023-01-06 21:28:02 -0500

    rewrote json parsing section to filter returns

--- Rocket-Launch-MagTag.ino
+++ Rocket-Launch-MagTag.ino
@@ -1,5 +1,3 @@
-
-
 /* 
 Function: Connnects to the launch library API and displays name date and description of
 the latest or upcoming launch on an E ink Display
@@ -10,15 +8,13 @@ Date Created: 12/17/22
 */
 
 #include <TimeLib.h>
-
 #include <ArduinoJson.h>
 #include <ArduinoJson.hpp> 
-
 #include "Adafruit_ThinkInk.h"
 #include "Anita_semi_square9pt7b.h"
 #include "Anita_semi_square8pt7b.h"
 #include "Bitmaps.h"
-
+#include <Adafruit_NeoPixel.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <WiFiManager.h>
@@ -33,13 +29,9 @@ Date Created: 12/17/22
 
 // 2.9" Grayscale for MagTag
 ThinkInk_290_Grayscale4_T5 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
-
 #define COLOR1 EPD_BLACK
 #define COLOR2 EPD_LIGHT
 #define COLOR3 EPD_DARK
-
-
-#define RSTButton 15
 
 
 const int UTC_Correction = -5; //Offset in housrs of current time zone from UTC
@@ -55,29 +47,52 @@ unsigned long btnA_init_time = 0;
 unsigned int Reset_Hold_Time = 10000; //number of milliseconds the button must be held to clear the saved configuration
 
 
+StaticJsonDocument<160> filter;
 
 
-WiFiClient client;
+WiFiManager wm;
+const char* config_str = "<p> Optional feature selection:</p>"
+                    "<input style='width: auto; margin: 0 10px 0 10px;' type='radio' id='option1' name='NeoPixel' value'NPX'><label for='option1'>Lights</label><br>";
+WiFiManagerParameter config_field(config_str);
+//WiFiClient client;
+
+
+Adafruit_NeoPixel *pixels;
+uint32_t StandardColor = pixels->ColorHSV(186,19,100);
+
 
 void setup() {
 
   pinMode(BUTTON_A, INPUT_PULLUP);
-  Serial.begin(115200);
-  delay(3000);
-  Serial.println("Next Launch Tag V0.1");
+
+  //Neopixel config
+  pixels = new Adafruit_NeoPixel(NEOPIXEL_NUM, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+  // The magtag has the ability to shutoff the neopixels for power savings. LOW = On
+  pinMode(NEOPIXEL_POWER, OUTPUT); 
+  digitalWrite(NEOPIXEL_POWER, NEOPIXEL_POWER_ON);
+  delay(200);
+  pixels->begin();
+  pixels->clear(); //make sure all pixels are off
+  pixels->show();
 
   display.begin(THINKINK_GRAYSCALE4);
 
-  WiFiManager wm;
+  Serial.begin(115200);
+  Serial.println("Next Launch Tag V0.1");
+ 
   wm.setClass("invert"); //set the config portal to darkl theme
+  wm.addParameter(&config_field);
+  wm.setSaveConfigCallback(saveParamsCallback);
   bool res;
   res = wm.autoConnect("LaunchTag");
+  wm.setWiFiAutoReconnect(true);
 
   if (!res) {
     Serial.println("Wifi Connection Failed");
     wm.resetSettings();
     //ESP.restart
   }
+ 
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -88,9 +103,14 @@ void setup() {
   Serial.println("Connected to WiFi");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  delay(3000);
 
 
+  //Create json document to filter the stream from the api. this reduces the amount of memory needed since there is a lot of information provided that we dont use.
+  JsonObject filter_results_0 = filter["results"].createNestedObject();
+  filter_results_0["window_start"] = true;
+  filter_results_0["status"]["id"] = true;
+  filter_results_0["rocket"]["configuration"]["name"] = true;
+  filter_results_0["mission"]["name"] = true;
 
 
   GetLaunch(); 
@@ -107,14 +127,13 @@ void ManualReset() {
   display.setCursor(0, 10);
   display.print("Wifi Reset.");
   display.setCursor(0, 30);
-  display.print("Please connect to SSID \"LaunchTag\" and enter WiFi credentials.");
+  display.print("Connect to SSID \"LaunchTag\" and enter WiFi credentials.");
   display.drawBitmap(100,58,Git_QR_Bitmap_Small,60,60, EPD_BLACK);
   display.display();
 
-  WiFiManager wmr;
-  wmr.resetSettings();
+  wm.resetSettings();
   bool res;
-  res = wmr.autoConnect("LaunchTag");
+  res = wm.autoConnect("LaunchTag");
 
   if (!res) {
     Serial.println("Wifi Connection Failed");
@@ -157,95 +176,107 @@ void loop() {
 
   if((millis() - Last_Update) > Update_Interval) //get updated launch info after a period of time has elapsed
   {
+    pixels->fill(StandardColor);
+    pixels->show();
     GetLaunch();
     Last_Update = millis();
+    delay(5000); //leave the lights on for 5 seconds after display updates
+    pixels->fill();
+    pixels->show();
   }
+
+  if(!)
 }
 
 
 void GetLaunch() {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient client;
-    client.begin("https://ll.thespacedevs.com/2.2.0/launch/upcoming/?limit=1&format=json&ordering=status__ids");
+    client.begin("https://ll.thespacedevs.com/2.2.0/launch/upcoming/?limit=10&format=json");
     int httpCode = client.GET();
 
     String payload = client.getString();
-    Serial.println(payload);
-
-    DynamicJsonDocument doc(4096);
-    DeserializationError error = deserializeJson(doc, payload);
+    //Serial.println(payload);
+    DynamicJsonDocument doc(3072);
+    DeserializationError error = deserializeJson(doc, payload, DeserializationOption::Filter(filter));
 
     if (error) {
       Serial.print("deserializeJson() failed: ");
       Serial.println(error.c_str());
+      display.clearBuffer();
+      display.setFont(&Anita_semi_square8pt7b);
+      display.setTextColor(EPD_BLACK);
+      display.setCursor(0, 15);
+      display.println("ERROR: JSON could not be deserialized.");
+      display.println(error.c_str());
+      display.display();
       return;
     }
 
-    int count = doc["count"];  
-    const char* next = doc["next"];
+    for (JsonObject result : doc["results"].as<JsonArray>()) {
 
-    JsonObject Results = doc["results"][0];
-    const char* Results_id = Results["id"];  // "bf91b6d9-f517-4198-843c-dc82dfa6f9b0"
-    const char* Results_url = Results["url"];
-    const char* Results_slug = Results["slug"];  // "falcon-9-block-5-starlink-group-4-37"
-    const char* Results_name = Results["name"];  // "Falcon 9 Block 5 | Starlink Group 4-37"
-
-    JsonObject status = Results["status"];
-    int status_id = status["id"];                            // 3
-    const char* status_name = status["name"];                // "Launch Successful"
-    const char* status_abbrev = status["abbrev"];            // "Success"
-    const char* status_description = status["description"];  // "The launch vehicle ...
-
-    const char* Results_last_updated = Results["last_updated"];  // "2022-12-18T00:58:06Z"
-    const char* Results_net = Results["net"];                    // "2022-12-17T21:32:00Z"
-    const char* Results_window_end = Results["window_end"];      // "2022-12-17T21:32:00Z"
-    const char* Results_window_start = Results["window_start"];  // "2022-12-17T21:32:00Z"
-    int Results_probability = Results["probability"];            // 60
-    const char* Results_holdreason = Results["holdreason"];      // nullptr
-    const char* Results_failreason = Results["failreason"];      // nullptr
-    // Results["hashtag"] is null
-
-    JsonObject Mission = Results["mission"];
-    const char* Mission_Name = Mission["name"];
-    const char* Mission_Description = Mission["description"];
-
-    Time_Correction(Results_window_start);
+      int result_status_id = result["status"]["id"]; // 1, 2, 2, 2, 2, 1, 2, 2, 2, 2
+      const char* result_window_start = result["window_start"]; // "2023-01-03T14:56:00Z", ...
+      const char* result_rocket_configuration_name = result["rocket"]["configuration"]["name"]; // "Falcon 9", ...
+      const char* result_mission_name = result["mission"]["name"]; // "Transporter 6 (Dedicated SSO ...
 
 
-    Serial.print("Launch name:");
-    Serial.println(Results_name);
+      if (result_status_id != 3 && result_status_id != 4 ) // id's 3 and 4 corrispond to a launch that has already happend
+      {
+        client.end(); // close the connection
 
-    char buffer[400];
-    sprintf(buffer,"NEXT LAUNCH: \nDate: %02d/%02d/%4d \nTime: %02d:%02d:%02d \n %s \n",month(), day(), year(), hour(), minute(), second(),Results_name );
+        Serial.printf("Status ID: %i \n", result_status_id);
+        Serial.printf("Window Start: %s \n", result_window_start);
+        Serial.printf("Rocket: %s \n", result_rocket_configuration_name);
+        Serial.printf("Mission name: %s \n",result_mission_name);
+
+        Time_Correction(result_window_start);
+        const char* meridiem = "";
+
+        if(isAM())
+        {
+          meridiem = "AM";
+        }
+        else
+        {
+          meridiem = "PM";
+        }
+
+        char Present_Buffer[300];
+        sprintf(Present_Buffer,"----------NEXT LAUNCH----------\nDate: %02d/%02d/%4d \nTime: %02d:%02d:%02d %s\nRocket: %s\nMission: %s",month(), day(), year(), hourFormat12(), minute(), second(), meridiem, result_rocket_configuration_name, result_mission_name);
+        display.clearBuffer();
+        display.setFont(&Anita_semi_square8pt7b);
+        display.setTextColor(EPD_BLACK);
+        display.setCursor(0, 15);
+        display.print(Present_Buffer);
+
+        display.display();
+        return; //found the data and displayed it we are done here
+
+      }
+
+      else
+      {
+        Serial.printf("Status ID indicates old launch: %i", result_status_id);
+      }
+
+    }
+
+  }  //end of connected
+
+  else 
+  {
+    Serial.println("No WiFi Connection");
     display.clearBuffer();
     display.setFont(&Anita_semi_square8pt7b);
     display.setTextColor(EPD_BLACK);
     display.setCursor(0, 15);
-    display.print(buffer);
-    /* 
-    display.print("Next Launch");
-    display.setCursor(0, 30);
-    display.print(Results_name);
-    display.setCursor(0, 60);
-    display.printf("Date: %02d/%02d/%4d %02d:%02d:%02d", month(), day(), year(), hour(), minute(), second());
-    */
+    display.println("ERROR: No WiFi connection.");
     display.display();
-
-
-    client.end();
-
-
-  }  //end of connected
-
-
-  else {
-    Serial.println("No WiFi Connection");
     return;
   }
 
-
 }  //GetLaunch end
-
 
 
 
@@ -265,4 +296,20 @@ Serial.print("Unix launch time in EST: ");
 Serial.println(new_unix);
 Serial.printf("%02d/%02d/%4d %02d:%02d:%02d", day(), month(), year(), hour(), minute(), second());
 
+}
+
+void saveParamsCallback()
+{
+  Serial.println("Get Params");
+  Serial.print("Selection: ");
+  Serial.println(getParam("NeoPixel"));
+}
+
+String getParam(String name) {
+  //read parameter from server, for customhmtl input
+  String value;
+  if (wm.server->hasArg(name)) {
+    value = wm.server->arg(name);
+  }
+  return value;
 }
